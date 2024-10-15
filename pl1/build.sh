9d1840d3c16e7e012824f8d0cc24de79f6239214
Susko3 <Susko3@protonmail.com>
2024-10-15 16:38:36 +0100

    Remove unsued <math.h>

--- pl1/build.sh
+++ pl1/build.sh
@@ -5,12 +5,10 @@ item=item3
 
 mkdir -p bin
 
-# -lm for <math.h> sqrt
-
 # items 1-4, 6
-gcc -Wall -O2 -D_REENTRANT $item.c jitter.c utils.c func.o -o bin/$item -lpthread -lrt -lm
+gcc -Wall -O2 -D_REENTRANT $item.c jitter.c utils.c func.o -o bin/$item -lpthread -lrt
 
 # Item 5
-#gcc -Wall -O2 -D_REENTRANT $item.c jitter.c utils.c func2.c -o bin/$item -lpthread -lrt -lm
+#gcc -Wall -O2 -D_REENTRANT $item.c jitter.c utils.c func2.c -o bin/$item -lpthread -lrt
 
 sudo ./bin/$item
--- pl1/jitter.c
+++ pl1/jitter.c
@@ -1,5 +1,3 @@
-#include <math.h>
-
 #include "jitter.h"
 
 // Modified from:
