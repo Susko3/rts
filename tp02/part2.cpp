58cc6ead2a89462b924635916567cce18e685b86
Susko3 <Susko3@protonmail.com>
2024-12-12 12:22:55 +0000

    use default params in part2.cpp

--- tp02/part2.cpp
+++ tp02/part2.cpp
@@ -133,10 +133,7 @@ void *preprocess_discard_thread(void *arg)
         pthread_mutex_unlock(state->loaded.mutex);
 
         lidar_data outbound{};
-        float forward = 30;
-        float side = 15;
-        float top = 2;
-        preprocess_discard(inflight, &outbound, forward, side, top);
+        preprocess_discard(inflight, &outbound);
         delete inflight;
 
         pthread_mutex_lock(state->preprocessed.mutex);
@@ -166,11 +163,6 @@ void *identify_driveable_thread(void *arg)
 {
     auto state = static_cast<struct state *>(arg);
 
-    float forward = 30;
-    float side = 15;
-    float maxDiff = 2;
-    float maxIncline = 0.10;
-
     while (running)
     {
         pthread_mutex_lock(state->preprocessed.mutex);
@@ -193,7 +185,7 @@ void *identify_driveable_thread(void *arg)
         pthread_mutex_unlock(state->preprocessed.mutex);
 
         lidar_data output;
-        identify_driveable(&inflight, &output, forward, side, maxDiff, maxIncline);
+        identify_driveable(&inflight, &output);
 
         std::cout << "Final data size: " << output.points.size() << std::endl;
 
